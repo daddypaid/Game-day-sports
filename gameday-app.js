73dfe6322d8dbf39a28f8432a6aa0be5ee238ef0
daddypaid <daddypaid@gmail.com>
2026-09-13 04:11:54 -0500

    Update gameday-app.js for Blackjack rollback

--- gameday-app.js
+++ gameday-app.js
@@ -1,7 +1,7 @@
 (() => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js?v=57', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
+      navigator.serviceWorker.register('./sw.js?v=58', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
     });
   }
 
