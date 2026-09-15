a701bd40d30c1877f387731214b5c9cb00240624
daddypaid <daddypaid@gmail.com>
2026-09-15 02:05:53 -0500

    Restore approved Blackjack five-button row (gameday-app.js)

--- gameday-app.js
+++ gameday-app.js
@@ -1,7 +1,7 @@
 (() => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js?v=61', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
+      navigator.serviceWorker.register('./sw.js?v=62', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
     });
   }
 
