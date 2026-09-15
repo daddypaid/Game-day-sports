e22356df971ed011de37ca509a659d8bce173115
daddypaid <daddypaid@gmail.com>
2026-09-15 03:11:56 -0500

    Move Blackjack balance clear of title (gameday-app.js)

--- gameday-app.js
+++ gameday-app.js
@@ -1,7 +1,7 @@
 (() => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js?v=68', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
+      navigator.serviceWorker.register('./sw.js?v=69', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
     });
   }
 
