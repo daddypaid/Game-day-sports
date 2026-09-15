01a836649d0c18eb40706b21dfd11337012960ff
daddypaid <daddypaid@gmail.com>
2026-09-15 02:47:58 -0500

    Add Blackjack return button only (gameday-app.js)

--- gameday-app.js
+++ gameday-app.js
@@ -1,7 +1,7 @@
 (() => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js?v=64', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
+      navigator.serviceWorker.register('./sw.js?v=65', { scope: './', updateViaCache: 'none' }).then(registration => registration.update()).catch(() => {});
     });
   }
 
